15ddf20440ee24d36e8150d9419f533afb488830
Patrick Hanford <hanfordpatrick@gmail.com>
2019-12-28 18:48:41 -0500

    improvement(footer): make footer content more defined
    This commit makes content in footer brighter and more defined. Includes phone number and
    professional email.
    
    re #3

--- src/components/navigation/FooterComponent.vue
+++ src/components/navigation/FooterComponent.vue
@@ -3,20 +3,22 @@
     <div class="flex flex-row justify-center border-moss shadow-lg border-t-2">
       <div class="flex flex-col items-center p-4">
         <h3 class="font-bold text-white text-md">
-          Patrick Hanford | CodeSpent
+          Patrick Hanford
         </h3>
         <h4 class="text-sm text-gray-600">
-          pat@phanford.me
+          (412) 585-2541
         </h4>
-        <h4 class="text-sm text-gray-600">
-          Pittsburgh, PA, US
-        </h4>
+        <div class="flex flex-row">
+          <h4 class="text-sm text-gray-600">
+            pat@phanford.me
+          </h4>
+        </div>
         <div class="flex flex-row">
           <ul class="container flex flex-row">
             <li
               v-for="social in socials"
               :key="social.name"
-              class="m-1 text-shell"
+              class="m-1 text-white"
             >
               <a
                 :href="social.link"
@@ -43,38 +45,32 @@ export default {
         {
           name: "Twitter",
           link: "https://twitter.com/codespent/",
-          icon: ["fab", "twitter"],
-          color: "#00aced"
+          icon: ["fab", "twitter"]
         },
         {
           name: "GitHub",
           link: "https://github.com/codespent/",
-          icon: ["fab", "github"],
-          color: "#fff"
+          icon: ["fab", "github"]
         },
         {
           name: "LinkedIn",
           link: "https://linkedin.com/in/phanford/",
-          icon: ["fab", "linkedin"],
-          color: "#2867b2"
+          icon: ["fab", "linkedin"]
         },
         {
           name: "Discord",
           link: "https://discord.gg/kfeQkV9/",
-          icon: ["fab", "discord"],
-          color: "#7289DA"
+          icon: ["fab", "discord"]
         },
         {
           name: "Twitch",
           link: "https://twitch.tv/codespent/",
-          icon: ["fab", "twitch"],
-          color: "#9146FF"
+          icon: ["fab", "twitch"]
         },
         {
           name: "Dev",
           link: "https://dev.to/codespent/",
-          icon: ["fab", "dev"],
-          color: "#77dd77"
+          icon: ["fab", "dev"]
         }
       ]
     };
