74678e58430f38de2e530392e5cf6b5fa577e1d8
Patrick Hanford <hanfordpatrick@gmail.com>
2019-12-31 02:56:53 -0500

    fix(navbar/blog): fix blog link in navbar
    Blog link in navbar from a blog post would navigate to /blog/post/postId/blog/ rather than /blog/.
    Changed blog links to use a named route to prevent this.

--- src/router.js
+++ src/router.js
@@ -1,39 +1,39 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
 
-import HomeView from '@/views/home/HomeView';
-import BlogView from '@/views/blog/BlogView';
-import BlogHomeView from '@/views/blog/BlogHomeView';
-import BlogPostView from '@/views/blog/BlogPostView';
-import blogService from '@/services/blogService';
+import HomeView from "@/views/home/HomeView";
+import BlogView from "@/views/blog/BlogView";
+import BlogHomeView from "@/views/blog/BlogHomeView";
+import BlogPostView from "@/views/blog/BlogPostView";
+import blogService from "@/services/blogService";
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  scrollBehavior (to, from, savedPosition) {
-    return { x: 0, y: 0 }
+  mode: "history",
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
   },
   routes: [
     {
-      path: '/',
+      path: "/",
       component: HomeView,
-      name: 'home',
+      name: "home"
     },
     {
-      path: '/blog/',
+      path: "/blog/",
       component: BlogView,
-      name: 'blog',
       children: [
         {
-          path: '/',
+          path: "/",
+          name: "blog",
           component: BlogHomeView,
-          name: 'blog-home',
+          name: "blog-home"
         },
         {
-          path: '/blog/post/:articleId/',
+          path: "/blog/post/:articleId/",
           component: BlogPostView,
-          name: 'blog-post',
+          name: "blog-post",
           async beforeEnter(to, from, next) {
             await blogService
               .fetchArticleById(to.params.articleId)
@@ -44,14 +44,14 @@ export default new Router({
                     break;
                   }
                   default: {
-                    next('error');
+                    next("error");
                     break;
                   }
                 }
               });
-          },
-        },
-      ],
-    },
-  ],
+          }
+        }
+      ]
+    }
+  ]
 });
